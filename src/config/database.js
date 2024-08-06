5b02b54fc12ecd7539f659595c7ec570ae4ed914
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2024-08-06 23:15:12 +0545

    updated config.json and db configuration

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,11 @@ import config from './config.json' assert { type: 'json' };;
 import { config as envConfig } from "./envConfig.js";
 
 const environment = envConfig.NODE_ENV;
-const { username, password, database, host, dialect } = config[environment];
+const { username, password, database, host, dialect, port } = config[environment];
 const sequelize = new Sequelize(database, username, password, {
     host,
     dialect,
+    port
 });
 
 export default sequelize;
--- src/migrations/20240730042148-create-listingImage-table.cjs
+++ src/migrations/20240730042148-create-listingImage-table.cjs
@@ -25,9 +25,6 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       },
-      id:{
-        type: Sequelize.INTEGER,
-      },
       caption: {
         type: Sequelize.STRING,
         allowNull: true,
