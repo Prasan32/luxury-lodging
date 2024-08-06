b57f26f0f6017cfc195b9a4e536fa8b832a10a50
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2024-08-06 23:56:38 +0545

    added body-parser for webhook

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createCustomer, createPaymentIntent, savePaymentInfo, getPaymentIntentInfo, handleWebhookResponses } from "../controllers/payment.controller.js";
 import { validate } from "../middlewares/validation.js";
 import { createCustomerSchema, createPaymentIntentSchema, savePaymentInfoSchema } from "../validationSchema/payment.js";
+import bodyParser from "body-parser";
 const router = Router();
 
 router.route('/createpaymentintent').post(validate(createPaymentIntentSchema), createPaymentIntent);
@@ -12,6 +13,6 @@ router.route('/createcustomer').post(validate(createCustomerSchema), createCusto
 
 router.route('/savepaymentinfo').post(validate(savePaymentInfoSchema), savePaymentInfo)
 
-router.route('/handlewebhookresponses').post(handleWebhookResponses)
+router.route('/handlewebhookresponses').post(bodyParser.raw({ type: 'application/json' }),handleWebhookResponses)
 
 export default router;
